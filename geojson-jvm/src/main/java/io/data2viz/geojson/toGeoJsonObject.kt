2b9173045e177dae6e933ee48d574a181d7032a9
gzoritchak <g.zoritchak@gmail.com>
2018-02-20 15:31:08 +0100

    Rename package to io.data2viz.geojson

---
+++ geojson-jvm/src/main/java/io/data2viz/geojson/toGeoJsonObject.kt
@@ -0,0 +1,8 @@
+package io.data2viz.geojson
+
+/**
+ * Parse the String as a GeoJsonObject
+ */
+actual fun String.toGeoJsonObject(): GeoJsonObject {
+	TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
