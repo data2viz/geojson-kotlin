48ceddfa934f8d354ec690fd660a3ba3c9ed2db0
gzoritchak <g.zoritchak@gmail.com>
2018-10-06 23:44:41 +0200

    chore(gradle): bintray WIP.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,21 @@
 buildscript {
     repositories {
         jcenter()
+        mavenCentral()
+        maven(uri("https://kotlin.bintray.com/kotlinx"))
+        maven(uri("https://plugins.gradle.org/m2/"))
     }
 
     dependencies {
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:${Versions.dokka_version}")
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:${Versions.bintray}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath("com.moowork.gradle:gradle-node-plugin:${Versions.gradle_node}")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:${Versions.dokka}")
     }
 }
 
-
-
 plugins {
-    kotlin("jvm" ) version("1.2.71") apply false
+    kotlin("jvm") version ("1.2.71") apply false
 }
 
 // apply the plugins on the child projects from here so that the extensions, configurations etc. that they define is
@@ -33,5 +37,14 @@ subprojects {
     repositories {
         jcenter()
         maven { url = uri("https://kotlin.bintray.com/kotlinx") }
+    }
+
+
+    apply(plugin = "com.jfrog.bintray")
+    apply(plugin = "maven")
+    apply(plugin = "maven-publish")
+
+    if(project.hasProperty("bintray.user")){
+        bintray
     }
 }
--- buildSrc/src/main/kotlin/extensions.kt
+++ buildSrc/src/main/kotlin/extensions.kt
@@ -1,26 +1,28 @@
 import org.gradle.api.Project
 
-val Project.kotlinVersion get() = Versions.kotlinVersion
+val Project.kotlinVersion get() = Versions.kotlin
 val Project.version get() = Versions.version
-val Project.dokka_version get() = Versions.dokka_version
+val Project.dokka_version get() = Versions.dokka
 val Project.group get() = Versions.group
-val Project.junit_version get() = Versions.junit_version
-val Project.bintray_version get() = Versions.bintray_version
-val Project.coroutines_version get() = Versions.coroutines_version
-val Project.gradle_node_version get() = Versions.gradle_node_version
-val Project.node_version get() = Versions.node_version
-val Project.npm_version get() = Versions.npm_version
+val Project.junit_version get() = Versions.junit
+val Project.bintray_version get() = Versions.bintray
+val Project.coroutines_version get() = Versions.coroutines
+val Project.gradle_node_version get() = Versions.gradle_node
+val Project.node_version get() = Versions.node
+val Project.npm_version get() = Versions.npm
 
 object Versions {
-    val kotlinVersion = "1.2.71"
-    val version = "0.6.0"
-    val dokka_version = "0.9.16"
     val group = "io.data2viz.geojson"
-    val junit_version = "4.12"
-    val bintray_version = "1.7.3"
-    val coroutines_version = "0.26.1"
-    val gradle_node_version = "1.2.0"
-    val node_version = "8.9.3"
-    val npm_version = "5.5.1"
+    val version = "0.6.1-SNAPSHOT"
+
+
+    val kotlin = "1.2.71"
+    val dokka = "0.9.16"
+    val junit = "4.12"
+    val bintray = "1.7.3"
+    val coroutines = "0.26.1"
+    val gradle_node = "1.2.0"
+    val node = "8.9.3"
+    val npm = "5.5.1"
 
 }
