5615c5b0c16994add12058fac1470b30ec38ab39
gzoritchak <g.zoritchak@gmail.com>
2020-11-30 12:23:39 +0100

    core: documentation

--- core/src/commonMain/kotlin/io/data2viz/geojson/GeoJson.kt
+++ core/src/commonMain/kotlin/io/data2viz/geojson/GeoJson.kt
@@ -1,11 +1,67 @@
 package io.data2viz.geojson
 
 
+/**
+ * Marker interface to indicate a GeoJson object. It can be
+ * a Geometry, a Feature or a FeatureCollection
+ */
+interface GeoJsonObject
+
+/**
+ * A feature contains a Geometry, an optional id, and optional properties.
+ */
+data class Feature(
+        val geometry: Geometry,
+        val id:Any? = null,
+        val properties: Any? = null
+) : GeoJsonObject
+
+/**
+ * A feature collection is an array of features.
+ */
+data class FeatureCollection(val features: Array<Feature>): GeoJsonObject
+
+
+interface Geometry : GeoJsonObject
+
+
+data class Point            (val coordinates: Position)     : Geometry
+data class MultiPoint       (val coordinates: Positions)    : Geometry
+data class LineString       (val coordinates: Line)         : Geometry
+data class MultiLineString  (val coordinates: Lines)        : Geometry
+data class Polygon          (val coordinates: Lines)        : Geometry {
+    val hasHoles = coordinates.size > 1
+}
+data class MultiPolygon     (val coordinates: Surface)      :Geometry
+data class GeometryCollection(val geometries: Array<Geometry>): Geometry
+
+
+/**
+ * Position is an alias on a DoubleArray that represents the coordinates
+ * in degrees for longitude (index = 0), latitude (index = 1) and altitude (meters).
+ * The altitude is not a mandatory information. The position can be 2 length array or
+ * a 3 length array (with altitude)
+ */
 typealias Position  = DoubleArray
+
+/**
+ * Type alias of an array of Positions
+ */
 typealias Positions = Array<Position>
+
+/**
+ * Type alias for an array of Positions
+ */
 typealias Line = Positions
+
+/**
+ * Type alias of an array of lines.
+ */
 typealias Lines     = Array<Line>
+
+
 typealias Surface   = Array<Lines>
+
 
 val Position.lon: Double
     get() = this[0]
@@ -16,42 +72,10 @@ val Position.lat: Double
 val Position.alt: Double?
     get() = if (size > 2) this[2] else null
 
-/**
- * Marker interface to indicate a GeoJson object. It can be
- * a Geometry, a Feature or a FeatureCollection
- */
-interface GeoJsonObject
-
-interface Geometry : GeoJsonObject
-
-
-data class Feature(
-        val geometry: Geometry,
-        val id:Any? = null,
-        val properties: Any? = null
-) : GeoJsonObject
-
-
-data class FeatureCollection(val features: Array<Feature>): GeoJsonObject
-
-data class Point(val coordinates: Position): Geometry
-
-data class MultiPoint(val coordinates: Positions): Geometry
-
-data class LineString(val coordinates: Line): Geometry
-
-data class MultiLineString(val coordinates: Lines): Geometry
-
-data class Polygon(val coordinates: Lines): Geometry {
-    val hasHoles = coordinates.size > 1
-}
-
-data class MultiPolygon(val coordinates: Surface):Geometry
-
-data class GeometryCollection(val geometries: Array<Geometry>): Geometry
 
 /**
- * Parse the String as a GeoJsonObject
+ * Parse the String as a GeoJsonObject. The implementation depends on
+ * the platform
  */
 expect fun String.toGeoJsonObject():GeoJsonObject
 
@@ -69,8 +93,14 @@ expect fun String.toGeoJsonObject():GeoJsonObject
 expect fun <T> String.toFeaturesAndProperties(extractFunction: FeatureProperties.() -> T): List<Pair<Feature, T>>
 //expect fun <T> String.toFeature(extract: FeatureProperties.() -> T): Pair<Feature, T>
 
+/**
+ * This class simplifies the access to feature properties. It should only
+ * be used as a way to deserialize the properties of a feature using
+ * the String.toFeaturesAndProperties function.
+ */
 expect class FeatureProperties {
     fun stringProperty(name: String): String
     fun intProperty(name: String): Int
     fun booleanProperty(name: String): Boolean
 }
+
